fd389ef7818dd17978be0ad9084b70bcdad303d0
Oleg Myalik <malikoleg1.23@gmail.com>
2025-09-02 00:11:00 +0300

    Simple search created

--- products/views.py
+++ products/views.py
@@ -1,15 +1,19 @@
 from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 from products.models import Products
+from products.utils import q_search
 
 #Контроллер страницы catalog
-def catalog(request, category_slug):
+def catalog(request, category_slug=None):
     page=request.GET.get('page', 1)
     on_sale=request.GET.get('on_sale', None)
     order_by=request.GET.get('order_by', None)
+    query=request.GET.get('q', None)
     
     if category_slug=='all':
         products_list=Products.objects.all()
+    elif query:
+        products_list=q_search(query)
     else:
         products_list=get_list_or_404(Products.objects.filter(category__slug=category_slug))
     
