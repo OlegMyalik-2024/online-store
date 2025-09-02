756c93a12e0b3dc03b4b955c84a96e85b7d5d028
Oleg Myalik <malikoleg1.23@gmail.com>
2025-09-02 23:11:55 +0300

    PosgreSql database connected & full text search

--- products/utils.py
+++ products/utils.py
@@ -1,8 +1,7 @@
 # Файл для сохранения рандомных дополнительных функций 
 
 from products.models import Products
-from django.db.models import Q 
-
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 
@@ -11,11 +10,6 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
-    keywords=[word for word in query.split() if len(word)>2]
-    q_objects=Q()
-    
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-        
-    return Products.objects.filter(q_objects)
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
--- shop/settings.py
+++ shop/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+from django.conf.global_settings import PASSWORD_HASHERS
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     
     'debug_toolbar',
     
@@ -82,8 +85,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'HelloMobile',
+        'USER': 'HelloMobile',
+        'PASSWORD': 'Hgggght97107',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
