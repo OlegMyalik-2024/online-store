eb5da2922e0fbfe106a81937eac750dcf0fca87b
Oleg Myalik <malikoleg1.23@gmail.com>
2025-09-03 00:26:09 +0300

    Full text search well done

--- products/utils.py
+++ products/utils.py
@@ -1,15 +1,42 @@
-# Файл для сохранения рандомных дополнительных функций 
+# Файл для сохранения рандомных дополнительных функций
 
 from products.models import Products
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
-
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchHeadline,
+)
 
 
 # Функция поиска
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+
+    result = (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color:yellow;">',
+            stop_sel="</span>",
+        )
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color:yellow;">',
+            stop_sel="</span>",
+        )
+    )
+
+    return result
