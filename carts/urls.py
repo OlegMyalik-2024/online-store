2492d4450087e124c17648e60b664c03602fea08
Oleg Myalik <malikoleg1.23@gmail.com>
2025-09-07 18:05:50 +0300

    Add to cart ajax request and json response

--- carts/urls.py
+++ carts/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
 from carts import views
+from django.urls import path
+
+
 
 app_name='carts' #инициализация простанства имен carts
 
 urlpatterns = [
-    path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
-    path('cart_change/<slug:product_slug>/', views.cart_change, name='cart_change'),
-    path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'),
+    path('cart_add/', views.cart_add, name='cart_add'),
+    path('cart_change/', views.cart_change, name='cart_change'),
+    path('cart_remove/', views.cart_remove, name='cart_remove'),
 ]
